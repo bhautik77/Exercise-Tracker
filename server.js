b4e46abf4ef68acdd84cf4afbc8655f0c2474a3a
Glitch (chisel-bejewled-organ) <none>
2022-04-19 04:59:37 +0000

    👔🌏 Checkpoint ./package.json:43926626/80 ./server.js:43926626/997

--- server.js
+++ server.js
@@ -2,6 +2,30 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const { Schema } = mongoose;
+
+const exerciseSchema = new Schema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: Date
+});
+
+const userSchema = new Schema({
+  username: String,
+});
+
+const exerciseSchema = new Schema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: Date
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 app.use(cors())
 app.use(express.static('public'))

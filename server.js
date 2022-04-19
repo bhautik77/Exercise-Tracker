c7d15788454ff5f1f49741d24cb668cffef9bcb4
Glitch (exercise-tracker-bhautik77) <none>
2022-04-19 14:07:24 +0000

    👵🚝 Checkpoint ./server.js:43926626/350 ./package.json:43926626/25

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+var mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
 
 app.use(cors())
 app.use(express.static('public'))
